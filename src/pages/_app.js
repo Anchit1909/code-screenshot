08476159751563119199cb96269be33f945b1cee
Anchit Sinha <anchit1909@gmail.com>
2023-09-04 00:04:48 +0530

    initial setup done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Toaster />
+      <Component {...pageProps} />
+    </>
+  );
 }
